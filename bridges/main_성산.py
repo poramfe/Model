51fe1e358fcbc3d255127960d904328044bd2d26
soundwave <45279161+CleberYoo@users.noreply.github.com>
2023-10-30 21:05:15 +0900

    Add files via upload

--- bridges/main_성산.py
+++ bridges/main_성산.py
@@ -7,50 +7,49 @@ import math
 class EuclideanDistTracker:
     def __init__(self):
         # Store the center positions of the objects
-        self.center_points = {}
+        self.centeroid_points = {}
         # Keep the count of the IDs
         # each time a new object id detected, the count will increase by one
         self.id_count = 0
 
 
-    def update(self, objects_rect):
+    def update(self, objects_rectangle):
         # Objects boxes and ids
-        objects_bbs_ids = []
+        objects_bounding_boxes_ids = []
 
         # Get center point of new object
-        for rect in objects_rect:
-            x, y, w, h = rect
-            cx = (x + x + w) // 2
-            cy = (y + y + h) // 2
+        for rectangle in objects_rectangle:
+            x, y, width, height = rectangle;    centeriod_of_x = (x + x + width) // 2;    centeroid_of_y = (y + y + height) // 2
 
             # Find out if that object was detected already
             same_object_detected = False
-            for id, pt in self.center_points.items():
-                dist = math.hypot(cx - pt[0], cy - pt[1])
+            for id, point in self.centeroid_points.items():
+                distance = math.hypot(centeriod_of_x - point[0], centeroid_of_y - point[1])
 
-                if dist < 25:
-                    self.center_points[id] = (cx, cy)
-                    print(self.center_points)
-                    objects_bbs_ids.append([x, y, w, h, id])
+                if distance < 25:
+                    self.centeroid_points[id] = (centeriod_of_x, centeroid_of_y)
+                    # print(self.centeroid_points)
+                    objects_bounding_boxes_ids.append([x, y, width, height, id])
                     same_object_detected = True
                     break
 
             # New object is detected we assign the ID to that object
             if same_object_detected is False:
-                self.center_points[self.id_count] = (cx, cy)
-                objects_bbs_ids.append([x, y, w, h, self.id_count])
+                self.centeroid_points[self.id_count] = (centeriod_of_x, centeroid_of_y)
+                objects_bounding_boxes_ids.append([x, y, width, height, self.id_count])
                 self.id_count += 1
 
         # Clean the dictionary by center points to remove IDS not used anymore
         new_center_points = {}
-        for obj_bb_id in objects_bbs_ids:
-            _, _, _, _, object_id = obj_bb_id
-            center = self.center_points[object_id]
+        for object_boundingbox_id in objects_bounding_boxes_ids:
+            _, _, _, _, object_id = object_boundingbox_id
+            center = self.centeroid_points[object_id]
             new_center_points[object_id] = center
 
         # Update dictionary with IDs not used removed
-        self.center_points = new_center_points.copy()
-        return objects_bbs_ids
+        self.centeroid_points = new_center_points.copy()
+
+        return objects_bounding_boxes_ids
 
 
 
@@ -61,6 +60,9 @@ upload_url = "http://localhost:4000/api/videos/upload"
 capture = cv2.VideoCapture(filename="./bridges/dataset/성산대교/2023-10-24 10-57-11oneway.mov")
 object_detector = cv2.createBackgroundSubtractorMOG2(history=1140, varThreshold=32, detectShadows=True)
 tracker = EuclideanDistTracker()
+
+frame_counter = 0
+total_id_10sec_ago = 0
 
 while True:
     return_value, frame = capture.read()
@@ -83,14 +85,20 @@ while True:
             object_location.append([x, y, w, h])
             # cv2.drawContours(image=frame_Region_of_Interest, contours=contour, contourIdx=-1, color=(255, 255, 255), thickness=5)
 
-    boxes_ids = tracker.update(objects_rect=object_location)
+    boxes_ids = tracker.update(objects_rectangle=object_location)
     for box_id in boxes_ids:
         x, y, w, h, id = box_id
         cv2.putText(img=frame_Region_of_Interest, text=str(id), org=(x, y), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 255, 255), thickness=3)
         cv2.rectangle(img=frame_Region_of_Interest, pt1=(x, y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=3)
 
+    frame_counter += 1
+    current_total_id = id
+    if frame_counter % 3600 == 0:
+        print(f"{(current_total_id - total_id_10sec_ago) / 1} cars per 1 minute")
+        total_id_10sec_ago = id
+
     cv2.imshow(winname="SungSan Bridge", mat=frame)
-    cv2.imshow(winname="mask", mat=MoG_mask)
+    # cv2.imshow(winname="mask", mat=MoG_mask)
 
     key = cv2.waitKey(delay=1)
     if key == 27: break
